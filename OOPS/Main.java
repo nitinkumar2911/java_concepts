e6613db10f78767ba28be0f7c6587e961bb9a256
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-08 14:56:46 +0530

    Modifying attribute

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -2,6 +2,7 @@ public class Main {
     int x = 5;
     public static void main(String[] args){
         Main myObj = new Main();
+        myObj.x = 25;
         System.out.println(myObj.x);
 
     }
