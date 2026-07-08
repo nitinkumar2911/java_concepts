7d9c1bdcab737bfbb28d041444cf1274019171f2
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-09 05:06:52 +0530

    use of this keyword in java

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -2,14 +2,26 @@ public class Main {
     int x = 5;
     int modelYear;
     String modelName;
-    public Main(int year, String name){
-        modelYear = year;
-        modelName = name ;
+    // public Main(int year, String name){
+    //     modelYear = year;
+    //     modelName = name ;
+    // }
+    public Main(String modelName){
+        this(2020, modelName);
+    }
+    public Main(int modelYear, String modelName){
+        this.modelYear = modelYear;
+        this.modelName = modelName;
+    }
+    public void printInfo(){
+        System.out.println(modelYear + " " + modelName);
     }
 
     
     public static void main(String[] args){
-       Main myCar = new Main(1969, "Mustang");
- System.out.println(myCar.modelYear + " " + myCar.modelName);
+      Main car1 = new Main("Corvette");
+      Main car2 = new Main(1969, "Mustang");
+      car1.printInfo();
+      car2.printInfo();
     }
 }
