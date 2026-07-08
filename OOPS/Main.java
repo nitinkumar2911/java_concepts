d40ab21fea385da1c2f983023845c1ab71f87ec8
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-08 17:25:19 +0530

    Access Methods with an object

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     int x = 5;
-     static void myMethod() {
-    System.out.println("Hello World!");
-     }
+    public void fullThrottle(){
+       System.out.println("The car is going as fast as it can!");  
+    }
+     public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
     public static void main(String[] args){
-       myMethod();
+       Main myCar = new Main();
+        myCar.fullThrottle();
+          myCar.speed(200);  
     }
 }
