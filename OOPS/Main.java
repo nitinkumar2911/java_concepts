565528c748702f0ffccbb6522b89b346aecfb203
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-08 15:02:51 +0530

    Using multiple objects

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,9 +1,11 @@
 public class Main {
-    final int x = 5;
+    int x = 5;
     public static void main(String[] args){
-        Main myObj = new Main();
-        myObj.x = 25;
-        System.out.println(myObj.x);
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        myObj2.x = 25;
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
 
     }
 }
