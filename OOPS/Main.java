e5c6e6af76fefa60749c78c9564b1b67486a0fde
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-08 15:08:56 +0530

    Calling a method

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     int x = 5;
-    String fname = "John";
-        String lname = "Doe";
-     int age = 24;
+     static void myMethod() {
+    System.out.println("Hello World!");
+     }
     public static void main(String[] args){
-        Main myObj = new Main();
-       System.out.println("Name: " + myObj.fname + " " + myObj.lname);
-    System.out.println("Age: " + myObj.age);
-
+       myMethod();
     }
 }
