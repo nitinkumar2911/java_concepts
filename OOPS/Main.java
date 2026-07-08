8002a5af66e334380892b4ff979b7539987d63c5
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-08 15:05:11 +0530

    Understand Multiple Attributes

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     int x = 5;
+    String fname = "John";
+        String lname = "Doe";
+     int age = 24;
     public static void main(String[] args){
-        Main myObj1 = new Main();
-        Main myObj2 = new Main();
-        myObj2.x = 25;
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.x);
+        Main myObj = new Main();
+       System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Age: " + myObj.age);
 
     }
 }
