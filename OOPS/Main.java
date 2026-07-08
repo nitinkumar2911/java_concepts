68b20b9698b9e361e120ee06da3187ce5e975678
NitinKumar1054 <nitin2911.in@gmail.com>
2026-07-09 05:18:26 +0530

    public and private modifier

--- OOPS/Main.java
+++ OOPS/Main.java
@@ -1,27 +1,11 @@
+class Person{
+    public String name = "John";
+    private int age = 30;
+}
 public class Main {
-    int x = 5;
-    int modelYear;
-    String modelName;
-    // public Main(int year, String name){
-    //     modelYear = year;
-    //     modelName = name ;
-    // }
-    public Main(String modelName){
-        this(2020, modelName);
-    }
-    public Main(int modelYear, String modelName){
-        this.modelYear = modelYear;
-        this.modelName = modelName;
-    }
-    public void printInfo(){
-        System.out.println(modelYear + " " + modelName);
-    }
-
-    
     public static void main(String[] args){
-      Main car1 = new Main("Corvette");
-      Main car2 = new Main(1969, "Mustang");
-      car1.printInfo();
-      car2.printInfo();
+     Person p = new Person();
+     System.out.println(p.name);
+     System.out.println(p.age);
     }
 }
