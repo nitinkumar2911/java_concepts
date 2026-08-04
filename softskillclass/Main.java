52484c8e6cd7f03b38554934aee679b9e3e44571
NitinKumar1054 <nitin2911.in@gmail.com>
2026-08-04 11:02:38 +0530

    correct code of binary search

--- softskillclass/Main.java
+++ softskillclass/Main.java
@@ -1,19 +1,25 @@
-class Main{
+class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,5,7,9,12,25,30};
-        int target = 9;
+
+        int[] arr = {1, 2, 5, 7, 9, 12, 25, 30};
+        int target = 2;
+
         int l = 0;
-        int r = arr.length;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(arr[mid] == target){
-                System.out.println("Element is found at index  " + mid);
+        int r = arr.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Element found at index " + mid);
                 return;
-            }if (arr[mid]>target){
-                l = mid;
-            }if(arr[mid]< target){
-                r = mid;
+            } else if (arr[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
+
+        System.out.println("Element not found");
     }
 }
